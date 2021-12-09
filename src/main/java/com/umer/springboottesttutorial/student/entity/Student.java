d9c299608077359d1937f5588b24f2630014f4ec
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-12-09 13:37:53 +0500

    Feature: Added dependency in pom, Create structure of Student entity and added application properties.

--- src/main/java/com/umer/springboottesttutorial/student/entity/Student.java
+++ src/main/java/com/umer/springboottesttutorial/student/entity/Student.java
@@ -1,5 +1,57 @@
 package com.umer.springboottesttutorial.student.entity;
 
-public class Student {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@ToString
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table
+public class Student {
+	
+	
+	@Id
+	@SequenceGenerator(
+			name="student_sequence",
+			sequenceName = "student_sequence",
+			allocationSize = 1
+	)
+	@GeneratedValue(
+			generator = "student_sequence",
+			strategy = GenerationType.SEQUENCE
+	)
+	private Long id;
+
+	
+	private String name;
+	
+	
+	private String email;
+	
+	
+	private Gender gender;
+	
+	
+
+    public Student(String name, String email, Gender gender) {
+        this.name = name;
+        this.email = email;
+        this.gender = gender;
+    }
 }
