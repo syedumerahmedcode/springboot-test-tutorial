18fa197b5481f16cbaf09ac4e91f6963dfe84ad1
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-12-11 17:28:45 +0500

    Feature: Added exception package with business related exceptions.

--- src/main/java/com/umer/springboottesttutorial/service/StudentService.java
+++ src/main/java/com/umer/springboottesttutorial/service/StudentService.java
@@ -18,4 +18,8 @@ public class StudentService {
 	public List<Student> getAllStudents(){
 		return studentRepository.findAll();
 	}
+	
+	public void addStudent(Student student) {
+		Boolean emailExists= studentRepository.selectExistsEmail(student.getEmail());
+	}
 }
