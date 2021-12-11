ce271a619e672c3465eff59cdfa8a81e9181a793
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-12-11 12:20:09 +0500

    Feature: Created a basic JPARepository for Student entity CRUD operations.

---
+++ src/main/java/com/umer/springboottesttutorial/student/repository/StudentRepository.java
@@ -0,0 +1,12 @@
+package com.umer.springboottesttutorial.student.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.umer.springboottesttutorial.student.entity.Student;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long>{
+
+}
