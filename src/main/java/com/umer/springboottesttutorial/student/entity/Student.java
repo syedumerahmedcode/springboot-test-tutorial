a788dc5950e9ab38d8a63398f48db9f9eacc5c52
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-12-11 12:13:04 +0500

    Feature: Added validations on Student entity.

--- src/main/java/com/umer/springboottesttutorial/student/entity/Student.java
+++ src/main/java/com/umer/springboottesttutorial/student/entity/Student.java
@@ -1,11 +1,17 @@
 package com.umer.springboottesttutorial.student.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
@@ -38,13 +44,17 @@ public class Student {
 	)
 	private Long id;
 
-	
+	@NotBlank
+	@Column(nullable = false)
 	private String name;
 	
-	
+	@Email
+	@Column(nullable = false, unique = true)
 	private String email;
 	
-	
+	@NotNull
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
 	private Gender gender;
 	
 	
