789ed603533ece1f5f5e434d7ccc461d09a1652a
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-12-11 12:37:03 +0500

    Feature: Added a method for finding all students.

--- src/main/java/com/umer/springboottesttutorial/service/StudentService.java
+++ src/main/java/com/umer/springboottesttutorial/service/StudentService.java
@@ -1,8 +1,21 @@
 package com.umer.springboottesttutorial.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.umer.springboottesttutorial.entity.Student;
+import com.umer.springboottesttutorial.repository.StudentRepository;
+
+import lombok.AllArgsConstructor;
+
 @Service
+@AllArgsConstructor
 public class StudentService {
 
+	private final StudentRepository studentRepository;
+	
+	public List<Student> getAllStudents(){
+		return studentRepository.findAll();
+	}
 }
