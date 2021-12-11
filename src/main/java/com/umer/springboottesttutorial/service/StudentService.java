2a99706b6b843993c33f790ec96b10414a0b6b83
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-12-11 17:36:59 +0500

    Feature: Created removeStudent() methid.

--- src/main/java/com/umer/springboottesttutorial/service/StudentService.java
+++ src/main/java/com/umer/springboottesttutorial/service/StudentService.java
@@ -1,13 +1,11 @@
 package com.umer.springboottesttutorial.service;
 
 import java.util.List;
-
 import org.springframework.stereotype.Service;
-
 import com.umer.springboottesttutorial.entity.Student;
 import com.umer.springboottesttutorial.exception.BadRequestException;
+import com.umer.springboottesttutorial.exception.StudentNotFoundException;
 import com.umer.springboottesttutorial.repository.StudentRepository;
-
 import lombok.AllArgsConstructor;
 
 @Service
@@ -15,17 +13,24 @@ import lombok.AllArgsConstructor;
 public class StudentService {
 
 	private final StudentRepository studentRepository;
-	
-	public List<Student> getAllStudents(){
+
+	public List<Student> getAllStudents() {
 		return studentRepository.findAll();
 	}
-	
+
 	public void addStudent(Student student) {
 		final String email = student.getEmail();
-		Boolean emailExists= studentRepository.selectExistsEmail(email);
-		if(emailExists) {
-			throw new BadRequestException("Email " + email + " taken");
+		Boolean emailExists = studentRepository.selectExistsEmail(email);
+		if (emailExists) {
+			throw new BadRequestException("Email " + email + " already taken.");
 		}
 		studentRepository.save(student);
+	}
+
+	public void deleteStudent(long studentId) {
+		if (!studentRepository.existsById(studentId)) {
+			throw new StudentNotFoundException("Student with id " + studentId + " does not exists.");
+		}
+		studentRepository.deleteById(studentId);
 	}
 }
