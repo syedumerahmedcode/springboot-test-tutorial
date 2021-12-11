6db011de5c328aab2dfccb6efbb35ff6db10bae4
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-12-11 12:31:46 +0500

    Feature: Added a query method for checking if email is empty.

--- src/main/java/com/umer/springboottesttutorial/repository/StudentRepository.java
+++ src/main/java/com/umer/springboottesttutorial/repository/StudentRepository.java
@@ -1,11 +1,23 @@
 package com.umer.springboottesttutorial.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.umer.springboottesttutorial.entity.Student;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
+	
+	@Query(
+			"SELECT CASE "
+					+ "WHEN COUNT(s) > 0 "
+					+ "THEN TRUE "
+					+ "ELSE FALSE "
+					+ "END "
+			+ "FROM Student s "
+			+ "WHERE s.email = ?1"
+			)
+	Boolean selectExistsEmail(String email);
 
 }
