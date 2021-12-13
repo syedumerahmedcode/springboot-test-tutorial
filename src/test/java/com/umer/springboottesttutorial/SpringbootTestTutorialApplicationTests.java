9fb38055018aa7b8662c9f3c8440bc1fbf0986f9
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-12-13 22:28:39 +0500

    Test: Created a basic test.

--- src/test/java/com/umer/springboottesttutorial/SpringbootTestTutorialApplicationTests.java
+++ src/test/java/com/umer/springboottesttutorial/SpringbootTestTutorialApplicationTests.java
@@ -1,5 +1,7 @@
 package com.umer.springboottesttutorial;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,8 +10,19 @@ class SpringbootTestTutorialApplicationTests {
 
 	Calculator underTest = new Calculator();
 
-	@Test
-	void contextLoads() {
+	@Test//Junit annotation
+	void itShouldAddTwoNumbers() {
+		// given
+		int numberOne=20;
+		int numberTwo=30;
+		
+		// when
+		int result= underTest.add(numberOne, numberTwo);
+		
+		// then
+		final int expected = 50;
+		assertThat(result).isEqualTo(expected);
+		
 	}
 
 	class Calculator {
