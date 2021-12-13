d3bb0fbfee9206fcf2ec83b72bb928a6531f6731
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-12-13 20:43:19 +0500

    Feature: Implemented controller class.

--- src/main/java/com/umer/springboottesttutorial/controller/StudentController.java
+++ src/main/java/com/umer/springboottesttutorial/controller/StudentController.java
@@ -1,8 +1,18 @@
 package com.umer.springboottesttutorial.controller;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.umer.springboottesttutorial.entity.Student;
 import com.umer.springboottesttutorial.service.StudentService;
 
 import lombok.AllArgsConstructor;
@@ -13,5 +23,22 @@ import lombok.AllArgsConstructor;
 public class StudentController {
 	
 	private final StudentService studentService;
+	
+
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return studentService.getAllStudents();
+    }
+
+    @PostMapping
+    public void addStudent(@Valid @RequestBody Student student) {
+        studentService.addStudent(student);
+    }
+
+    @DeleteMapping(path = "{studentId}")
+    public void deleteStudent(
+            @PathVariable("studentId") Long studentId) {
+        studentService.deleteStudent(studentId);
+    }
 
 }
