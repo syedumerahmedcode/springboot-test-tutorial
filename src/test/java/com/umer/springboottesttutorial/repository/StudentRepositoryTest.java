b31f6466e5ba93ed4f37739191d0657ba37df6ed
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-12-14 10:10:21 +0500

    Test: Created a simple test for test itShouldCheckIfStudentExistsByEmail().

--- src/test/java/com/umer/springboottesttutorial/repository/StudentRepositoryTest.java
+++ src/test/java/com/umer/springboottesttutorial/repository/StudentRepositoryTest.java
@@ -1,5 +1,31 @@
 package com.umer.springboottesttutorial.repository;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.umer.springboottesttutorial.entity.Gender;
+import com.umer.springboottesttutorial.entity.Student;
+
 public class StudentRepositoryTest {
+	
+	@Autowired
+	private StudentRepository underTest;
+	
+	@Test
+	void itShouldCheckIfStudentExistsByEmail() {
+		// given
+		String email="some.email@gmail.com";
+		Student student=new Student("Alice", email, Gender.FEMALE);
+		
+		// when
+		boolean expected= underTest.selectExistsEmail(email);		
+		
+		
+		// then
+		assertThat(expected).isTrue();
+		
+	}
 
 }
