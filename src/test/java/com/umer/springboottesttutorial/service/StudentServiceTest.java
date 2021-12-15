9d3fc436fd0fdc589b40897aa83febbdaa8c6a89
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-12-15 11:36:42 +0500

    Test: Added more code for testing service. Also, added notes in the readme file for Mocking.

--- src/test/java/com/umer/springboottesttutorial/service/StudentServiceTest.java
+++ src/test/java/com/umer/springboottesttutorial/service/StudentServiceTest.java
@@ -1,10 +1,21 @@
 package com.umer.springboottesttutorial.service;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import com.umer.springboottesttutorial.repository.StudentRepository;
+
 public class StudentServiceTest {
 
+	private StudentRepository studentRepository; 
+	private StudentService underTest;
+	
+	@BeforeEach
+	void setUp() {
+		underTest=new StudentService(studentRepository);
+	}
+	
 	@Test
 	void getAllStudents() {
 		
