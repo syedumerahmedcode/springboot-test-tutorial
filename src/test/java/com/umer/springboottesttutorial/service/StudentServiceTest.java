8d0bda2bb865f42a95e49ac2ce321f19bdb855d7
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-12-15 11:25:04 +0500

    Test: Added empty test methods which will be implemented later.

--- src/test/java/com/umer/springboottesttutorial/service/StudentServiceTest.java
+++ src/test/java/com/umer/springboottesttutorial/service/StudentServiceTest.java
@@ -1,5 +1,24 @@
 package com.umer.springboottesttutorial.service;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
 public class StudentServiceTest {
 
+	@Test
+	void getAllStudents() {
+		
+	}
+	
+	@Test
+	@Disabled
+	void addStudent() {
+		
+	}
+	
+	@Test
+	@Disabled
+	void deleteStudent() {
+		
+	}
 }
