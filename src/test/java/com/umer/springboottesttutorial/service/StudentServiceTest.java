de9240a846cd9d0970cff37b1b3ca168eef41a5a
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-12-15 11:45:21 +0500

    Test: Implemented setUp() and tearDown() methods in service test class.

--- src/test/java/com/umer/springboottesttutorial/service/StudentServiceTest.java
+++ src/test/java/com/umer/springboottesttutorial/service/StudentServiceTest.java
@@ -1,35 +1,51 @@
 package com.umer.springboottesttutorial.service;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import com.umer.springboottesttutorial.repository.StudentRepository;
 
 public class StudentServiceTest {
 
-	private StudentRepository studentRepository; 
+	@Mock
+	private StudentRepository studentRepository;
+
+	private AutoCloseable autoCloseable;
 	private StudentService underTest;
-	
+
 	@BeforeEach
 	void setUp() {
-		underTest=new StudentService(studentRepository);
+		/**
+		 * If we have more than one mock, it will initialize all the mocks in this test
+		 * class.
+		 */
+		autoCloseable = MockitoAnnotations.openMocks(this);
+		underTest = new StudentService(studentRepository);
 	}
-	
+
+	@AfterEach
+	void tearDown() throws Exception {
+		autoCloseable.close();
+	}
+
 	@Test
 	void getAllStudents() {
-		
+
 	}
-	
+
 	@Test
 	@Disabled
 	void addStudent() {
-		
+
 	}
-	
+
 	@Test
 	@Disabled
 	void deleteStudent() {
-		
+
 	}
 }
